de53fb181251a4223362cec558819606fd765bdf
gamemanuel <themathnight@gmail.com>
2025-09-20 20:13:35 -0700

    fixed driver controll and added robot drive with spark maxes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl/DriverControll.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl/DriverControll.java
@@ -3,9 +3,10 @@ package org.firstinspires.ftc.teamcode.DriverControl;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
 import org.firstinspires.ftc.teamcode.RobotClass;
 
-@TeleOp(name = "ONTOS", group = "driving")
+@TeleOp(name = "ONTOS Combined", group = "driving")
 public class DriverControll extends OpMode {
 
     RobotClass robot;
@@ -17,16 +18,32 @@ public class DriverControll extends OpMode {
     double[] movePower = new double[6];
     boolean[] servoAt90 = new boolean[6];
 
-    // Single boolean for gamepad button states
+    // Last state for each button
     boolean lastAState = false;
-    boolean lastDpadDown = false;
+    boolean lastBState = false;
+    boolean lastXState = false;
+    boolean lastYState = false;
+    boolean lastRBState = false;
+    boolean lastRTState = false;
+    boolean lastDpadUpState = false;
+    boolean lastDpadDownState = false;
+    boolean lastDpadLeftState = false;
+    boolean lastDpadRightState = false;
+    boolean lastLBState = false;
+    boolean lastLTState = false;
 
     // A single constant for the target encoder value
-    static final int OPEN_POSITION = 7 * 20;
+    static final int OPEN_POSITION = 7 * 28;
 
     @Override
     public void init() {
         robot = new RobotClass(hardwareMap);
+
+        // Reverse one side of the drivetrain for tank drive
+        robot.frontRight.setDirection(CRServo.Direction.REVERSE);
+        robot.middleRight.setDirection(CRServo.Direction.REVERSE);
+        robot.backRight.setDirection(CRServo.Direction.REVERSE);
+
         // Initialize all servos to the closed position (0.0)
         for (int i = 0; i < 6; i++) {
             robot.gunFillServos[i].setPosition(0.0);
@@ -35,7 +52,7 @@ public class DriverControll extends OpMode {
 
     @Override
     public void start() {
-        // Reset and set the run mode for all motors
+        // Reset and set the run mode for all gun motors
         for (int i = 0; i < 6; i++) {
             robot.gunFireMotors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             robot.gunFireMotors[i].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -44,42 +61,69 @@ public class DriverControll extends OpMode {
 
     @Override
     public void loop() {
+        // --- Drivetrain Logic ---
+        double leftPower = gamepad1.left_stick_y;
+        double rightPower = gamepad1.right_stick_y;
+
+        robot.frontLeft.setPower(leftPower);
+        robot.middleLeft.setPower(leftPower);
+        robot.backLeft.setPower(leftPower);
+
+        robot.frontRight.setPower(rightPower);
+        robot.middleRight.setPower(rightPower);
+        robot.backRight.setPower(rightPower);
+
+        // --- Gun Logic ---
         // Read gamepad states once per loop
         boolean currentAState = gamepad1.a;
-        boolean currentDpadDown = gamepad1.dpad_down;
+        boolean currentBState = gamepad1.b;
+        boolean currentXState = gamepad1.x;
+        boolean currentYState = gamepad1.y;
+        boolean currentRBState = gamepad1.right_bumper;
+        boolean currentRTState = gamepad1.right_trigger > 0.5; // Use a threshold for triggers
+        boolean currentDpadUpState = gamepad1.dpad_up;
+        boolean currentDpadDownState = gamepad1.dpad_down;
+        boolean currentDpadLeftState = gamepad1.dpad_left;
+        boolean currentDpadRightState = gamepad1.dpad_right;
+        boolean currentLBState = gamepad1.left_bumper;
+        boolean currentLTState = gamepad1.left_trigger > 0.5; // Use a threshold for triggers
 
-        // --- Toggle logic for all guns with button presses ---
-        // A button toggles the motors
-        if (currentAState && !lastAState) {
-            for (int i = 0; i < 6; i++) {
-                valveOpen[i] = !valveOpen[i];
-                targetPosition[i] = valveOpen[i] ? OPEN_POSITION : 0;
-                movePower[i] = valveOpen[i] ? 1.0 : -1.0;
-                moving[i] = true;
-            }
-        }
+        // Trigger logic for each gun's fire motor
+        handleMotorToggle(0, currentAState, lastAState); // Gun 1 Fire
+        handleMotorToggle(1, currentBState, lastBState); // Gun 2 Fire
+        handleMotorToggle(2, currentXState, lastXState); // Gun 3 Fire
+        handleMotorToggle(3, currentYState, lastYState); // Gun 4 Fire
+        handleMotorToggle(4, currentRBState, lastRBState); // Gun 5 Fire
+        handleMotorToggle(5, currentRTState, lastRTState); // Gun 6 Fire
+
+        // Trigger logic for each gun's fill servo
+        handleServoToggle(0, currentDpadUpState, lastDpadUpState); // Gun 1 Fill
+        handleServoToggle(1, currentDpadDownState, lastDpadDownState); // Gun 2 Fill
+        handleServoToggle(2, currentDpadLeftState, lastDpadLeftState); // Gun 3 Fill
+        handleServoToggle(3, currentDpadRightState, lastDpadRightState); // Gun 4 Fill
+        handleServoToggle(4, currentLBState, lastLBState); // Gun 5 Fill
+        handleServoToggle(5, currentLTState, lastLTState); // Gun 6 Fill
+
+        // Update the last state for each button
         lastAState = currentAState;
+        lastBState = currentBState;
+        lastXState = currentXState;
+        lastYState = currentYState;
+        lastRBState = currentRBState;
+        lastRTState = currentRTState;
+        lastDpadUpState = currentDpadUpState;
+        lastDpadDownState = currentDpadDownState;
+        lastDpadLeftState = currentDpadLeftState;
+        lastDpadRightState = currentDpadRightState;
+        lastLBState = currentLBState;
+        lastLTState = currentLTState;
 
-        // D-pad Down toggles the servos
-        if (currentDpadDown && !lastDpadDown) {
-            for (int i = 0; i < 6; i++) {
-                servoAt90[i] = !servoAt90[i];
-                if (servoAt90[i]) {
-                    robot.gunFillServos[i].setPosition(1);
-                } else {
-                    robot.gunFillServos[i].setPosition(0.0);
-                }
-            }
-        }
-        lastDpadDown = currentDpadDown;
-
-        // --- Movement and telemetry logic for each gun ---
+        // Movement and telemetry logic for each gun
         for (int i = 0; i < 6; i++) {
             int position = robot.gunFireMotors[i].getCurrentPosition();
 
             if (moving[i]) {
                 if (valveOpen[i]) {
-                    // Opening: move forward until we reach or exceed target
                     if (position < targetPosition[i]) {
                         robot.gunFireMotors[i].setPower(movePower[i]);
                     } else {
@@ -87,7 +131,6 @@ public class DriverControll extends OpMode {
                         moving[i] = false;
                     }
                 } else {
-                    // Closing: move backward until we reach or go below target
                     if (position > targetPosition[i]) {
                         robot.gunFireMotors[i].setPower(movePower[i]);
                     } else {
@@ -98,18 +141,39 @@ public class DriverControll extends OpMode {
                     }
                 }
             } else {
-                // Manual control for all guns
-                robot.gunFireMotors[i].setPower(gamepad1.right_stick_y);
+                // If a gun is not "firing," it has no manual control
+                // You can add manual control for a single gun with a different button here if you want
+                robot.gunFireMotors[i].setPower(0);
             }
 
-            // Telemetry for each individual gun (for debugging)
             telemetry.addData("Gun " + (i + 1) + " Encoder Position", position);
-            telemetry.addData("Gun " + (i + 1) + " Target", targetPosition[i]);
-            telemetry.addData("Gun " + (i + 1) + " Valve Open?", valveOpen[i]);
-            telemetry.addData("Gun " + (i + 1) + " Moving?", moving[i]);
             telemetry.addData("Gun " + (i + 1) + " Servo Position", robot.gunFillServos[i].getPosition());
         }
 
+        // Add drivetrain telemetry
+        telemetry.addData("Left Drivetrain Power", leftPower);
+        telemetry.addData("Right Drivetrain Power", rightPower);
+
         telemetry.update();
+    }
+
+    private void handleMotorToggle(int gunIndex, boolean currentState, boolean lastState) {
+        if (currentState && !lastState) {
+            valveOpen[gunIndex] = !valveOpen[gunIndex];
+            targetPosition[gunIndex] = valveOpen[gunIndex] ? OPEN_POSITION : 0;
+            movePower[gunIndex] = valveOpen[gunIndex] ? 1.0 : -1.0;
+            moving[gunIndex] = true;
+        }
+    }
+
+    private void handleServoToggle(int gunIndex, boolean currentState, boolean lastState) {
+        if (currentState && !lastState) {
+            servoAt90[gunIndex] = !servoAt90[gunIndex];
+            if (servoAt90[gunIndex]) {
+                robot.gunFillServos[gunIndex].setPosition(1);
+            } else {
+                robot.gunFillServos[gunIndex].setPosition(0.0);
+            }
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClass.java
@@ -1,44 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class RobotClass {
-    public DcMotorEx gun1Fire, gun2Fire, gun3Fire, gun4Fire, gun5Fire, gun6Fire;
-    public Servo gun1Fill, gun2Fill, gun3Fill, gun4Fill, gun5Fill, gun6Fill;
+    // Drivetrain motors as CRServos
+    public CRServo frontLeft, frontRight, middleLeft, middleRight, backLeft, backRight;
 
-    public RobotClass(HardwareMap hardwareMap){
+    // Public arrays to hold all gun motor and servo objects
+    public DcMotorEx[] gunFireMotors = new DcMotorEx[6];
+    public Servo[] gunFillServos = new Servo[6];
 
-        // left side of robot
-        // Motors
-        gun1Fire = hardwareMap.get(DcMotorEx.class, "gun1Fire");
-//        gun2Fire = hardwareMap.get(DcMotorEx.class, "gun2Fire");
-//        gun3Fire = hardwareMap.get(DcMotorEx.class, "gun3Fire");
-//
-//        // Servos
-        gun1Fill = hardwareMap.get(Servo.class, "gun1Fill");
-//        gun2Fill = hardwareMap.get(Servo.class, "gun2Fill");
-//        gun3Fill = hardwareMap.get(Servo.class, "gun3Fill");
-//
-//        // right side of robot
-//        // Motors
-//        gun4Fire = hardwareMap.get(DcMotorEx.class, "gun4Fire");
-//        gun5Fire = hardwareMap.get(DcMotorEx.class, "gun5Fire");
-//        gun6Fire = hardwareMap.get(DcMotorEx.class, "gun6Fire");
-//
-//        // Servos
-//        gun4Fill = hardwareMap.get(Servo.class, "gun4Fill");
-//        gun5Fill = hardwareMap.get(Servo.class, "gun5Fill");
-//        gun6Fill = hardwareMap.get(Servo.class, "gun6Fill");
+    public RobotClass(HardwareMap hardwareMap) {
+        // Initialize Drivetrain
+        frontLeft = hardwareMap.get(CRServo.class, "frontLeftSparkMax");
+        frontRight = hardwareMap.get(CRServo.class, "frontRightSparkMax");
+        middleLeft = hardwareMap.get(CRServo.class, "middleLeftSparkMax");
+        middleRight = hardwareMap.get(CRServo.class, "middleRightSparkMax");
+        backLeft = hardwareMap.get(CRServo.class, "backLeftSparkMax");
+        backRight = hardwareMap.get(CRServo.class, "backRightSparkMax");
 
-        // robot configuration for test chassis currently commented out
-        // backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        // frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Use a loop to initialize all gun hardware components by name
+        for (int i = 0; i < 6; i++) {
+            String motorName = "gun" + (i + 1) + "Fire";
+            gunFireMotors[i] = hardwareMap.get(DcMotorEx.class, motorName);
 
+            String servoName = "gun" + (i + 1) + "Fill";
+            gunFillServos[i] = hardwareMap.get(Servo.class, servoName);
+        }
     }
-
 }
